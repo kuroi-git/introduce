388483e3dfa80f8b580c1e2d07547f46226ab11e
kuroi-git <149265808+kuroi-git@users.noreply.github.com>
2024-02-22 15:59:14 +0900

    240221

---
+++ introduction/js/select.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", () => {
+  // ボタンにイベントリスナーを設定
+  console.log("h1");
+
+  const buttons = document.querySelectorAll(".filter-button");
+  buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+      const tag = button.getAttribute("data-tag");
+      filterElements(tag);
+    });
+  });
+});
+
+function filterElements(tag) {
+  console.log("h1");
+  const elements = document.querySelectorAll(".element");
+  elements.forEach((element) => {
+    const tags = element.getAttribute("data-tags").split(" ");
+    if (tags.includes(tag)) {
+      element.style.display = "";
+    } else {
+      element.style.display = "none";
+    }
+  });
+}
